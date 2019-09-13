6e270dcedd5430b6a94f10ad18a0fa34009cf1b0
Jmasterson513 <jmasterson5130@gmail.com>
2019-09-13 16:58:15 -0400

    Added Copyright and Second Part

---
+++ group09/Project01/State.py
@@ -0,0 +1,10 @@
+#Copyright 2019 Joe Masterson, Cassidy Carter, and Alfred Stephenson II
+
+import Interface.py as Interface
+
+class State:
+    def __init__(self):
+        self.
+
+    def state(self, param):
+        
