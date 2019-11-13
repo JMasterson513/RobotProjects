efa415bb4a20ef301de1b87c433e3231bf3081b6
Jmasterson513 <jmasterson5130@gmail.com>
2019-11-13 15:32:21 -0500

    PD Controller returns values

--- group09/Project03/test3.py
+++ group09/Project03/test3.py
@@ -5,6 +5,7 @@ import time
 positive_velocity = 170
 
 button = False
+side = False
 
 #Set point varibale
 set_point= 13
@@ -14,9 +15,11 @@ KP =  1
 KD =  .2
 
 #Sampling  Time
-sampling_time= .005
+sampling_time= .05
 
-#target = 45
+sleep_time = 0.025
+
+error_list = [0]
 
 class test:
 	
@@ -36,29 +39,39 @@ class test:
                 button = not button
 
 	def stop(self):
-	    self.State.driveDirect(0,0)          
-	
-	#def wallFollow(self):
-	    #while True:
+	    self.State.driveDirect(0,0)         
+        
+        def whichState(self):
+            global side
+            right_state = self.State.readRightBumper()
+            left_state = self.State.readLeftBumper()
+            side = True if right_state > left_state else False
+       
+        def newErrorCalc(self): 
+            if (side):
+                error = self.State.readRightBumper() - set_point
+                error_list.append(error)
+            else:
+                error = self.State.readLeftBumper() - set_point
+                error_list.append(error)
 
-	def calculateLeftError(self):
-	    return self.State.readLeftBumper() - set_point
+        def PDController(self):
+            time.sleep(sleep_time)
+            self.newErrorCalc()
+            U_p = KP * (error_list[-1])
+            #print("U_p: {}".format(Up))
+            U_d = KD * ((error_list[-1] - error_list[-2]) / sampling_time)
+            U_ = U_p + U_d
+            return U_
 
-	def calculateRightError(self):
-	    return self.State.readRightBumper() - set_point
-	
-	def calculatePController(self):
-	    return KP*(self.calculatedLeftError())
-
-	def calculatedDController(self):
-	    return KD*(self.calculatedLeftError()/sampling_time)
-
+        
 roomba = test()
-button = False
 while True:
-	time.sleep(1)
+        roomba.whichState()
+        print(roomba.PDController())
 	#print ("Left Error Expected: {}".format(roomba.State.readLeftBumper() - set_point))
-	#print("Left Error Recieved: {}".format(roomba.calculateLeftError()))
-	print("Right Error Expected: {}".format(roomba.State.readRightBumper() - set_point))
-	print("Right Error Recieved: {}".format(roomba.calculateRightError()))
+        #roomba.newErrorCalc()
+	#print("Left Error Recieved: {}".format(error_list[-1]))
+	#print("Right Error Expected: {}".format(roomba.State.readRightBumper() - set_point))
+	#print("Right Error Recieved: {}".format(roomba.calculateRightError()))
 roomba.stop()
