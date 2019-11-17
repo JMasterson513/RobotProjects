2e032cd238645e721e3a4208e72bcdc3d863d2d4
Alfred0606 <alfred@AlfredsLaptop.localdomain>
2019-11-17 12:24:22 -0500

    Worked on center bumper

--- group09/Project03/test3.py
+++ group09/Project03/test3.py
@@ -1,5 +1,6 @@
 from State import State
 import time
+import math
 
 # Default speed for the roomba
 positive_velocity = 170
@@ -14,9 +15,9 @@ KP = 1
 KD =  .2
 
 #Sampling  Time
-sampling_time= .075
+sampling_time= .085
 
-sleep_time = 0.025
+sleep_time = 0.035
 
 error_list = [0]
 
@@ -58,7 +59,7 @@ class test:
             #else:
             error = self.State.readRightBumper() - set_point
             error_list.append(error)
-            print "error calculated"
+            
 
         def PDController(self):
             time.sleep(sleep_time)
@@ -67,22 +68,29 @@ class test:
             #print("U_p: {}".format(Up))
             U_d = KD * ((error_list[-1] - error_list[-2]) / sampling_time)
             U = U_p + U_d
-            print "u returned"
             return U
+
+        def centerWall(self):
+            wall=self.State.readCenterBumper()
+            if(wall):
+                time.sleep(.1)
+                self.State.driveDirect(170,-170)
+                time.sleep((math.radians(45))/(((170*2)/235)))
         
         def driveController(self):
             #side = False
             while True:
                 U_current = self.PDController()
+                wall = self.State.readCenterBumper()
                 print U_current
+                print wall
+                self.centerWall()
                 if(U_current  < 0):
                     self.State.driveDirect(-170,170)
                     time.sleep(sleep_time)
                 elif(U_current > 0):
                     self.State.driveDirect(170,-170)
                     time.sleep(sleep_time)
-                #elif(U_current ==0):    
-                    #self.State.driveDirect(-170,170) if side else self.State.driveDirect(170,-170)
                 self.drive()
                                 
 roomba = test()
