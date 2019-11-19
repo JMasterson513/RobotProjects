b317d27c19be9703de2656db04e893bc0c2acb0d
Alfred0606 <alfred@AlfredsLaptop.localdomain>
2019-11-18 19:49:38 -0500

    Revert "Wrote README" Revered Readme This reverts commit e6eaa90b78755cd335cf914054de13becd272387.

--- group09/Project03/State.py
+++ group09/Project03/State.py
@@ -53,6 +53,7 @@ class State:
 
     def readButton(self):
         self.Interface.send(struct.pack('BB', query, button_packet))
+        #time.sleep(0.015)
         returned_string = self.Interface.read(1)
         button_state = struct.unpack('B', returned_string)[0]
         return bool(button_state & 0x01)
@@ -97,12 +98,11 @@ class State:
 roomba = State()
 roomba.state(128)
 roomba.state(131)
-while True:
-    #print("Button: {}".format(roomba.readButton()))
+#while True:
     #time.sleep(.1)
     #print("Center: {}".format(roomba.readCenterBumper()))
     #time.sleep(.5)
-    print("Right: {}".format(roomba.readRightBumper()))
+    #print("Right: {}".format(roomba.readRightBumper()))
 	#print " "
 	#print "end of loop"
 
--- group09/Project03/test3.py
+++ group09/Project03/test3.py
@@ -1,7 +1,6 @@
 from State import State
 import time
 import math
-import threading
 
 # Default speed for the roomba
 positive_velocity = 170
@@ -32,27 +31,41 @@ class test:
 	def drive(self):
 	    self.State.driveDirect(positive_velocity, positive_velocity)
 	
+	#works
 	def readButton(self):
             global button
-            while True:
-                button_state = self.State.readButton()
-                time.sleep(.125)
-                #print("Button State: {}".format(self.State.readButton()))
-                if(button_state):
-                    button = not button
-                    print("Button: {}".format(button))
+            button_state = self.State.readButton()
+            if(button_state):
+                button = not button
 
 	def stop(self):
 	    self.State.driveDirect(0,0)         
         
+        def whichState(self):
+            right_state = self.State.readRightBumper()
+            print("Right State: {}".format(right_state))
+            left_state = self.State.readLeftBumper()
+            print("Left State: {}".format(left_state))
+            if(right_state > left_state):
+                return True
+            else:
+                return False
+
         def newErrorCalc(self): 
+            #side = self.whichState()
+            #if (side):
+                #error = self.State.readRightBumper() - set_point
+                #error_list.append(error)
+            #else:
             error = self.State.readRightBumper() - set_point
             error_list.append(error)
             
+
         def PDController(self):
             time.sleep(sleep_time)
             self.newErrorCalc()
             U_p = KP * (error_list[-1])
+            #print("U_p: {}".format(Up))
             U_d = KD * ((error_list[-1] - error_list[-2]) / sampling_time)
             U = U_p + U_d
             return U
@@ -60,7 +73,6 @@ class test:
         def centerWall(self):
             wall=self.State.readCenterBumper()
             if(wall):
-                print("Center Bumper Read")
                 time.sleep(.1)
                 self.State.driveDirect(170,-170)
                 time.sleep((math.radians(45))/(((170*2)/235)))
@@ -68,40 +80,28 @@ class test:
         def driveController(self):
             #side = False
             while True:
-                while button:
-                    U_current  = self.PDController()
-                    wall = self.State.readCenterBumper()
-                    print(U_current)
-                    if(not button):
-                        break
-                    self.centerWall()
-                    if(U_current < -100):
-                        print("Turn towards 1")
-                        self.State.driveDirect(-180,170)
-                        time.sleep(sleep_time)
-                    if(U_current > -100 and U_current < 0):
-                        print("Turm towards 2")
-                        self.State.driveDirect(-170,170)
-                        time.sleep(sleep_time)
-                    if(U_current > 100):
-                        print("Turn out 1")
-                        self.State.driveDirect(180,-170)
-                        time.sleep(sleep_time)
-                    elif(U_current < 100 and U_current > 0):
-                        print("Turn out 2")
-                        self.State.driveDirect(170,-170)
-                        time.sleep(sleep_time)
-                    self.drive()
-                self.State.driveDirect(0, 0)            
-
+                U_current = self.PDController()
+                wall = self.State.readCenterBumper()
+                print U_current
+                print wall
+                self.centerWall()
+                if(U_current  < 0):
+                    self.State.driveDirect(-170,170)
+                    time.sleep(sleep_time)
+                elif(U_current > 0):
+                    self.State.driveDirect(170,-170)
+                    time.sleep(sleep_time)
+                self.drive()
+                                
 roomba = test()
-button = False
-
-button_thread = threading.Thread(target=roomba.readButton)
-drive_thread = threading.Thread(target=roomba.driveController)
-
-button_thread.start()
-drive_thread.start()
-
-button_thread.join()
-drive_thread.join()
+while True:
+        #print(roomba.whichState())
+        #print(roomba.PDController())
+        roomba.driveController()
+        #print(roomba.PDController())
+	#print ("Left Error Expected: {}".format(roomba.State.readLeftBumper() - set_point))
+        #roomba.newErrorCalc()
+	#print("Left Error Recieved: {}".format(error_list[-1]))
+	#print("Right Error Expected: {}".format(roomba.State.readRightBumper() - set_point))
+	#print("Right Error Recieved: {}".format(roomba.calculateRightError()))
+roomba.stop()
